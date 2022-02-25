565c37c686378f6759afd61e8fa2bbfffc3940b3
Monica Rao <98614625+monica18496@users.noreply.github.com>
2022-02-25 10:55:13 -0500

    Add files via upload

---
+++ CC3-2.bat
@@ -0,0 +1,1 @@
+"C:/Users/Monica Rao/PycharmProjects/CodingChallenge3" CC3-2.py My name is Monica I am from India I am a graduate student
---
+++ CC3-2.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def main(arg):
+    print("My 1st argument: " + str("My name is Monica"))
+main(sys.argv[0])
+
+def main(arg):
+    print("My 2nd argument: " + str("I am from India"))
+main(sys.argv[0])
+
+def main(arg):
+    print("My 3rd argument: " + str("I am a graduate student"))
+main(sys.argv[0])
+
+
+
+
+
+
+
